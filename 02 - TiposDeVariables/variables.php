8ebcbfd7345d46e9107317baeb812dd8f82e3079
juanPabloCesarini <jpablocesarini@gmail.com>
2019-06-28 13:40:35 -0300

    Tipos de variables

---
+++ 02 - TiposDeVariables/variables.php
@@ -0,0 +1,23 @@
+<?php
+/* Cuando se declaran las variables tienen que tenter un valor inicial, no pueden quedar vacios.
+*se pueden igualar a null, en el ejemplo comienza null luego toma el valor 7 y eso se suma al 8
+*y muestra 15
+
+*/
+$varNull= null;
+$varNull=7;
+
+echo $varNull+8;
+
+//
+$entero = 4;//integer
+$decimal = 5.23; //double
+$buleano = true; // boolean puede ser true (verdadero) o false (falso)
+$cadena = "Hola Mundo"; // String
+$caracter = "a"; // char
+$vector = array("juan","pablo", "pedro"); // array
+$vector_asociativo = array("nombre"=>"juan", "estadoCivil" => "soltero", "nombre"=>"natalia", "estadoCivil"=>"casada");
+//$objeto = new object(); variable compuesta en POO un obejto se construye a partir de una clase con sus atributos y mètodos
+
+
+?>
