03ce7dc62719f302acb9862dafd0e5456aa52ca3
juanPabloCesarini <jpablocesarini@gmail.com>
2019-06-29 17:26:20 -0300

    Operador Condicional

---
+++ 04 - TiposDeOperadores/operadorCondicional.php
@@ -0,0 +1,11 @@
+<?php
+//Es una manera de escribir mas corto un if con una sola condición
+$edad=12;
+$mensaje = $edad >=18 ? "Es mayor de edad"."<br>":"Es menor de edad"."<br>";
+echo $mensaje;
+
+$edad=20;
+$mensaje = $edad >=18 ? "Es mayor de edad"."<br>":"Es menor de edad"."<br>";
+echo $mensaje;
+
+?>
