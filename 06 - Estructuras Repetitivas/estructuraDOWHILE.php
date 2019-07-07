f0e135ac343db58feffaf12ee66590c5450b1108
juanPabloCesarini <jpablocesarini@gmail.com>
2019-07-07 17:20:59 -0300

    Estructura DOWHILE

---
+++ 06 - Estructuras Repetitivas/estructuraDOWHILE.php
@@ -0,0 +1,16 @@
+<?php
+// la diferencia con el WHILE es que ahora por lo menow una vez se ejecuta el código
+
+$i =0;
+do {
+    echo $i . "<br>";
+    $i++;
+}while ($i<=10);
+echo "========================="."<br>";
+$i =10;
+do {
+    echo $i . "<br>";
+    $i++;
+}while ($i<10);
+
+?>
