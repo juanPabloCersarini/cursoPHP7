a4c5b9f3ff94c31ed9a854cc0b02b0ca828f1b46
juanPabloCesarini <jpablocesarini@gmail.com>
2019-07-22 15:24:16 -0300

    Conexion a la base con PDO y orientado a objeto

--- 20 - CRUDPDO/conexion.php
+++ 20 - CRUDPDO/conexion.php
@@ -1,6 +1,34 @@
 <?php
-    class Cnx{
-        public 
+    class Conexion{
+        public $cnx = null;
+
+        function __constructor(){
+            try {
+                $this->cnx  = $this->getConnection();
+
+            } catch (PDOException $e) {
+                echo $e -> getMessage();
+            }
+        }
+
+        private function getConnection(){
+            $host ="localhost";
+            $user = "root";
+            $pass = "";
+            $db = "basecursophp7";
+            $charset ="utf8";
+            $opt = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
+
+            $pdo = new pdo("mysql:host={$host};
+                            dname={$db};
+                            charset={$charset};",
+                            $user,
+                            $pass,
+                            $opt);
+            
+            $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        }
     }
 
 ?>
